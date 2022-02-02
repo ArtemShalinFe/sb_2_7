581e147f00db12d6c5dd142009fe08071ec279fd
Artem Shalin <a.shalin@rtinform.ru>
2022-02-02 20:52:35 +0300

    Initial Commit

---
+++ sb_2_7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sb_2_7
+//
+//  Created by Артем ШАЛИН on 02.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
